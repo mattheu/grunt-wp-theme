3e096655349191bd89f22904fc769ceba6cd65a8
Matth_eu <matthew@matth.eu>
2013-08-27 13:59:56 +0100

    remove languages

--- root/functions.php
+++ root/functions.php
@@ -27,22 +27,6 @@ function {%= prefix %}_is_dev() {
 }
 
 /**
-* Set up theme defaults and register supported WordPress features.
-*
-* @uses load_theme_textdomain() For translation/localization support.
-*
-* @since 0.1.0
-*/
-function {%= prefix %}_setup() {
-
-	/**
-	 * Makes {%= title %} available for translation.
-	 *
-	 * Translations can be added to the /lang directory.
-	 * If you're building a theme based on {%= title %}, use a find and replace
-	 * to change '{%= prefix %}' to the name of your theme in all template files.
-	 */
-	load_theme_textdomain( '{%= prefix %}', get_template_directory() . '/languages' );
 
 }
 add_action( 'after_setup_theme', '{%= prefix %}_setup' );
