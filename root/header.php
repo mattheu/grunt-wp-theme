6c667c3b83f980d79a184911988c7ab31a102000
Matth_eu <matthew@matth.eu>
2013-08-27 14:00:10 +0100

    header from gm-base

--- root/header.php
+++ root/header.php
@@ -8,21 +8,29 @@
  */
 
 ?><!DOCTYPE html>
-<!--[if IE 7 ]><html <?php language_attributes(); ?> class="no-js oldie ie7"><![endif]-->
-<!--[if IE 8 ]><html <?php language_attributes(); ?> class="no-js oldie ie8"><![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<!--[if IE 7]><html class="ie ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]><html class="ie ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
 <head>
 
- 	<!-- no-js or js Helper class to help prevent FOUC -->
+ 	<?php // no-js/js class. Help prevent FOUC. Can remove if Modernizr is used. ?>
 	<script type="text/javascript">(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title><?php bloginfo('name'); ?> <?php wp_title( '|' ); ?></title>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 	<?php wp_head(); ?>
 
+	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/externals/html5.js" type="text/javascript"></script>
+	<![endif]-->
+
 </head>
 
-<body <?php body_class(); ?>>
+<body>
